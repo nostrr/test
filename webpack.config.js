7015f6d31eed2f9f310429c5967465fc1cdd7775
Алексей <lukavnev@gmail.com>
2020-05-13 10:39:40 +0300

    add less

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,13 @@ module.exports = {
     output: {
         path: __dirname + '/public',
         filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.less$/,
+                loader: 'style-loader!css-loader!less-loader'
+            }
+        ]
     }
 }
