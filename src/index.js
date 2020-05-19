64e4288249d8d72f1fdb82debff0736623223cd5
Алексей <lukavnev@gmail.com>
2020-05-19 16:41:58 +0300

    state

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-import React, {PropTypes} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            text : ''
+        }
+    }
+
     static propTypes = {
-        btnText: PropTypes.string.isRequired,
+        btnText: React.PropTypes.string.isRequired,
         h1text: React.PropTypes.string.isRequired,
         newArray: React.PropTypes.array.isRequired
     };
@@ -16,13 +24,20 @@ class App extends React.Component {
         console.log('Button clicked!', event.target);
     };
 
+    inputOnChange(event){
+        console.log('Event', event.target.value);
+        this.setState({text : event.target.value});
+    }
+
     render() {
         console.log('array',this.props.newArray);
         return (
             <div style={{backgroundColor: 'red '}} className='test'>
+                <h1>Hello</h1>
                 <h1>{this.props.h1text}</h1>
                 <h3>It's really working!</h3>
                 <button onClick={this.btnOnClick}>{this.props.btnText }</button>
+                <input type='text' value={this.state.text} onChange={this.inputOnChange.bind(this)}/>
             </div>
         );
     }
